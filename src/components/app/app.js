e1ebe61c5e7d236fa0321e1ca45e42275ff86d77
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-06-30 12:46:33 +0300

    load

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,8 +3,10 @@ import Header from "../header";
 import Aside from "../aside";
 import Main from "../main";
 import AsideRight from "../aside-right";
+import Footer from "../footer";
 
 import "./app.css";
+
 
 export default class App extends Component {
     state = {
@@ -15,11 +17,12 @@ export default class App extends Component {
         return (
             <>
                 <Header />
-                <section className="d-flex justify-content-around">
+                <section className="d-flex justify-content-around p-2">
                     <Aside />
                     <Main />
                     <AsideRight />
                 </section>
+                <Footer />
             </>
         );
     }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,9 +7,9 @@ function Main() {
 
     return(
         <main className="col-6">
-            <article>
-                <h4>Какая-то очень важная статья/новость</h4>
-                <div>
+            <article className="card border-secondary mb-3">
+                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+                <div className="card-body">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
@@ -17,9 +17,29 @@ function Main() {
                     officia deserunt mollit anim id est laborum.
                 </div>
             </article>
-            <article>
-                <h4>Какая-то очень важная статья/новость</h4>
-                <div>
+            <article className="card border-secondary mb-3">
+                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+                <div className="card-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                    officia deserunt mollit anim id est laborum.
+                </div>
+            </article>
+            <article className="card border-secondary mb-3">
+                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+                <div className="card-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                    officia deserunt mollit anim id est laborum.
+                </div>
+            </article>
+            <article className="card border-secondary mb-3">
+                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+                <div className="card-body">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
